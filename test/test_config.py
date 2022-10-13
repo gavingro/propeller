654b23de117bf4fbd9f3fcd399878489495f7661
Gavin Grochowski <grochowskigavin@gmail.com>
2022-10-12 22:06:13 -0700

    Add YAML config support and tests.

---
+++ test/test_config.py
@@ -0,0 +1,37 @@
+import os
+import pytest
+import yaml
+
+from src.config import read_yaml_from
+
+
+@pytest.fixture
+def good_yaml_at_path():
+    # Save YAML to file
+    good_document = {
+            'a': 1,
+            'b': {
+                    'c': {
+                        'd': 3
+                    },
+                    'e': {
+                        'f': 4
+                    }
+                }
+    }
+    document_path = "good_document.yml"
+    with open(document_path, "w") as file:
+        yaml.dump(good_document, file)
+    # Pass filepath to tests.
+    yield document_path
+    # Cleanup temporary YAML file.
+    os.remove(document_path)
+
+
+def test_read_good_yaml(good_yaml_at_path):
+    document = read_yaml_from(good_yaml_at_path)
+    assert type(document) == dict
+    assert document["a"] == 1
+    assert type(document["b"]) == dict
+    assert type(document["b"]["c"]) == dict
+    assert document["b"]["e"]["f"] == 4
